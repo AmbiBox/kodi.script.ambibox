30471a0ca5ca2c3f1c7cd5bd30f0730288088739
Nicolas Morel <nmr.morel@gmail.com>
2013-11-18 22:17:46 +0100

    formatting

--- script.ambibox/default.py
+++ script.ambibox/default.py
@@ -1,12 +1,10 @@
-
 #Modules General
-import os,AmbiBox
-from sys import argv
+import AmbiBox
 
 # Modules XBMC
 import xbmc, xbmcgui, xbmcaddon
 
-__settings__ = xbmcaddon.Addon( "script.ambibox" )
+__settings__ = xbmcaddon.Addon("script.ambibox")
 __language__ = __settings__.getLocalizedString
 #########################################################################################################
 ## BEGIN
@@ -15,17 +13,17 @@ ambibox = AmbiBox.AmbiBox(__settings__.getSetting("host"), int(__settings__.getS
 ambibox.connect()
 menu = ambibox.getProfiles()
 menu.append(__language__(32021))
-off = len(menu)-1
+off = len(menu) - 1
 quit = False
 while not quit:
-	selected = xbmcgui.Dialog().select(__language__(32020), menu)
-	if selected != -1:
-		ambibox.lock()
-		if (off == int(selected)):
-			ambibox.turnOff()
-		else:
-			ambibox.turnOn()
-			ambibox.setProfile(menu[selected])
-		ambibox.unlock
-	quit = True
+    selected = xbmcgui.Dialog().select(__language__(32020), menu)
+    if selected != -1:
+        ambibox.lock()
+        if off == int(selected):
+            ambibox.turnOff()
+        else:
+            ambibox.turnOn()
+            ambibox.setProfile(menu[selected])
+        ambibox.unlock
+    quit = True
 ambibox.disconnect()
--- script.ambibox/service.py
+++ script.ambibox/service.py
@@ -1,34 +1,37 @@
-﻿
-#Modules General
-import os,AmbiBox
+﻿#Modules General
+import os
 import time
 from sys import argv
+
+import AmbiBox
+
 
 # Modules XBMC
 import xbmc, xbmcgui, xbmcaddon
 
-__settings__ = xbmcaddon.Addon( "script.ambibox" )
+__settings__ = xbmcaddon.Addon("script.ambibox")
 __language__ = __settings__.getLocalizedString
 
-#########################################################################################################
-## BEGIN
-#########################################################################################################
+
 def notification(text):
-	import os.path
-	text = text.encode('utf-8')
-	icon=__settings__.getAddonInfo( "icon" )
-	smallicon = icon.encode( "utf-8" )
-	if (__settings__.getSetting("notification")  == 'true'):
-		xbmc.executebuiltin('Notification(AmbiBox,'+text+',3000,' + smallicon + ')')
+    import os.path
+
+    text = text.encode('utf-8')
+    icon = __settings__.getAddonInfo("icon")
+    smallicon = icon.encode("utf-8")
+    if __settings__.getSetting("notification") == 'true':
+        xbmc.executebuiltin('Notification(AmbiBox,' + text + ',3000,' + smallicon + ')')
+
 
 def setProfile(enable, profile):
-	if (enable == 'true'):
-		notification(__language__(32032)%profile)
-		ambibox.turnOn()
-		ambibox.setProfile(profile)
-	else:
-		notification(__language__(32031))
-		ambibox.turnOff()
+    if enable == 'true':
+        notification(__language__(32032) % profile)
+        ambibox.turnOn()
+        ambibox.setProfile(profile)
+    else:
+        notification(__language__(32031))
+        ambibox.turnOff()
+
 
 print "service AmbiBox"
 notification(__language__(32030))
@@ -36,31 +39,31 @@ print "service AmbiBox connect"
 ambibox = AmbiBox.AmbiBox(__settings__.getSetting("host"), int(__settings__.getSetting("port")))
 ambibox.connect()
 oldstatus = -1
-while (not xbmc.abortRequested):
-	newstatus = 0
-	player = xbmc.Player()
-	audioIsPlaying = player.isPlayingAudio()
-	videoIsPlaying = player.isPlayingVideo()
-	if videoIsPlaying:
-		newstatus = 1
-	if audioIsPlaying:
-		newstatus = 2
-	if (oldstatus!=newstatus):
-		oldstatus = newstatus
-		ambibox.lock()
-		if (newstatus == 0):
-			setProfile(__settings__.getSetting("default_enable"),__settings__.getSetting("default_profile"))
-		if (newstatus == 1):
-			setProfile(__settings__.getSetting("video_enable"),__settings__.getSetting("video_profile"))
-		if (newstatus == 2):
-			setProfile(__settings__.getSetting("audio_enable"),__settings__.getSetting("audio_profile"))
-		ambibox.unlock()
-	time.sleep(1)
-	
+while not xbmc.abortRequested:
+    newstatus = 0
+    player = xbmc.Player()
+    audioIsPlaying = player.isPlayingAudio()
+    videoIsPlaying = player.isPlayingVideo()
+    if videoIsPlaying:
+        newstatus = 1
+    if audioIsPlaying:
+        newstatus = 2
+    if oldstatus != newstatus:
+        oldstatus = newstatus
+        ambibox.lock()
+        if newstatus == 0:
+            setProfile(__settings__.getSetting("default_enable"), __settings__.getSetting("default_profile"))
+        if newstatus == 1:
+            setProfile(__settings__.getSetting("video_enable"), __settings__.getSetting("video_profile"))
+        if newstatus == 2:
+            setProfile(__settings__.getSetting("audio_enable"), __settings__.getSetting("audio_profile"))
+        ambibox.unlock()
+    time.sleep(1)
+
 # set off
 notification(__language__(32031))
 print "set status off for AmbiBox"
 ambibox.lock()
 ambibox.turnOff()
-ambibox.unlock
+ambibox.unlock()
 ambibox.disconnect()
