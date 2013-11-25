70548725fcaa4785003913ad7dce992c45394879
Nicolas Morel <nmr.morel@gmail.com>
2013-11-25 21:39:11 +0100

    Convert the addon into a service and set the default profile at startup

--- script.ambibox/default.py
+++ script.ambibox/default.py
@@ -1,30 +1,144 @@
+#Modules General
+import os
+import sys
+import mmap
+
 # Modules XBMC
 import xbmc
 import xbmcgui
 import xbmcaddon
 
-#Modules Ambibox
+# Modules AmbiBox
 import AmbiBox
 
+__addon__ = xbmcaddon.Addon()
+__cwd__ = __addon__.getAddonInfo('path')
+__scriptname__ = __addon__.getAddonInfo('name')
 __settings__ = xbmcaddon.Addon("script.ambibox")
 __language__ = __settings__.getLocalizedString
 
+__resource__ = xbmc.translatePath(os.path.join(__cwd__, 'resources', 'lib'))
 
-ambibox = AmbiBox.AmbiBox(__settings__.getSetting("host"), int(__settings__.getSetting("port")))
-ambibox.connect()
-menu = ambibox.getProfiles()
-menu.append(__language__(32021))
-off = len(menu) - 1
-end = False
-while not end:
-    selected = xbmcgui.Dialog().select(__language__(32020), menu)
-    if selected != -1:
-        ambibox.lock()
-        if off == int(selected):
-            ambibox.turnOff()
+sys.path.append(__resource__)
+
+from Media import *
+media = Media()
+
+
+def notification(text):
+    text = text.encode('utf-8')
+    icon = __settings__.getAddonInfo("icon")
+    smallicon = icon.encode("utf-8")
+    if __settings__.getSetting("notification") == 'true':
+        xbmc.executebuiltin('Notification(AmbiBox,' + text + ',1000,' + smallicon + ')')
+
+
+def debug(msg):
+    xbmc.log("### [%s] - %s" % (__scriptname__, msg,), level=xbmc.LOGDEBUG)
+
+
+def info(msg):
+    xbmc.log("### [%s] - %s" % (__scriptname__, msg,), level=xbmc.LOGNOTICE)
+
+
+class CapturePlayer(xbmc.Player):
+    def __init__(self):
+        debug("service AmbiBox connect")
+        self.ambibox = AmbiBox.AmbiBox(__settings__.getSetting("host"), int(__settings__.getSetting("port")))
+        self.ambibox.connect()
+        self.setProfile(__settings__.getSetting("default_enable"), __settings__.getSetting("default_profile"))
+
+    def setProfile(self, enable, profile):
+        self.ambibox.lock()
+        if enable == 'true':
+            notification(__language__(32032) % profile)
+            self.ambibox.turnOn()
+            self.ambibox.setProfile(profile)
         else:
-            ambibox.turnOn()
-            ambibox.setProfile(menu[selected])
-        ambibox.unlock()
-    end = True
-ambibox.disconnect()
+            notification(__language__(32031))
+            self.ambibox.turnOff()
+        self.ambibox.unlock()
+
+    def onPlayBackStarted(self):
+        if self.isPlayingAudio():
+            self.setProfile(__settings__.getSetting("audio_enable"), __settings__.getSetting("audio_profile"))
+
+        if self.isPlayingVideo():
+            self.setProfile(__settings__.getSetting("video_enable"), __settings__.getSetting("video_profile"))
+
+            capture = xbmc.RenderCapture()
+
+            infos = media.getInfos(self.getPlayingFile())
+            width = infos[0]
+            height = infos[1]
+            ratio = infos[2]
+            notification("%d => %dx%d" % (ratio, width, height))
+
+            self.inDataMap = mmap.mmap(0, width * height * 4 + 11, 'AmbiBox_XBMC_SharedMem', mmap.ACCESS_WRITE)
+
+            capture.capture(width, height, xbmc.CAPTURE_FLAG_CONTINUOUS)
+
+            while self.isPlayingVideo():
+                capture.waitForCaptureStateChangeEvent()
+                if capture.getCaptureState() == xbmc.CAPTURE_STATE_DONE:
+                    self.inDataMap.seek(0)
+                    seeked = self.inDataMap.read_byte()
+                    if ord(seeked) == 248:
+                        # width
+                        self.inDataMap[1] = chr(width & 0xff)
+                        self.inDataMap[2] = chr((width >> 8) & 0xff)
+                        # height
+                        self.inDataMap[3] = (chr(height & 0xff))
+                        self.inDataMap[4] = (chr((height >> 8) & 0xff))
+                        # aspect ratio
+                        self.inDataMap[5] = (chr(int(ratio * 100)))
+                        # image format
+                        fmt = capture.getImageFormat()
+                        if fmt == 'RGBA':
+                            self.inDataMap[6] = (chr(0))
+                        elif fmt == 'BGRA':
+                            self.inDataMap[6] = (chr(1))
+                        else:
+                            self.inDataMap[6] = (chr(2))
+                        image = capture.getImage()
+                        length = len(image)
+                        # datasize
+                        self.inDataMap[7] = (chr(length & 0xff))
+                        self.inDataMap[8] = (chr((length >> 8) & 0xff))
+                        self.inDataMap[9] = (chr((length >> 16) & 0xff))
+                        self.inDataMap[10] = (chr((length >> 24) & 0xff))
+                        # data
+                        self.inDataMap[11:(11+length)] = str(image)
+                        # write first byte to indicate we finished writing the data
+                        self.inDataMap[0] = (chr(240))
+
+            self.inDataMap.close()
+            self.inDataMap = None
+
+    def onPlayBackStopped(self):
+        self.setProfile(__settings__.getSetting("default_enable"), __settings__.getSetting("default_profile"))
+        if self.inDataMap is not None:
+            self.inDataMap.close()
+            self.inDataMap = None
+
+    def close(self):
+        self.ambibox.lock()
+        self.ambibox.turnOff()
+        self.ambibox.unlock()
+        self.ambibox.disconnect()
+        if self.inDataMap is not None:
+            self.inDataMap.close()
+            self.inDataMap = None
+
+debug("service AmbiBox")
+notification(__language__(32030))
+
+player = CapturePlayer()
+while not xbmc.abortRequested:
+    xbmc.sleep(1000)
+
+# set off
+notification(__language__(32031))
+debug("set status off for AmbiBox")
+player.close()
+player = None
